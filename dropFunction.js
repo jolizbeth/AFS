a09eac97b0f27e4f4e52fe9768b56d5e17d26b8c
JolizbethBonilla <jolizbeth98@gmail.com>
2019-12-17 13:19:31 -0500

    Preferences : Dropdown time

--- dropFunction.js
+++ dropFunction.js
@@ -1,18 +1,16 @@
 // function for dropdown if box is checked off
-function dropFunction() {
-  // Get the checkbox
-  var checkBox = document.getElementById("a1").value;
-  // Get the dropdown
-  var myDrop = document.getElementById("myDropdown").value;
-  if (myDrop.style.display == "block"){
-    myDrop.style.display == "none";
+
+  function dropFunction() {
+    // Get the checkbox
+    var checkBox = document.getElementById("a1");
+    // Get the dropdown
+    var myDrop = document.getElementById("myDropdown");
+    
+    // If the checkbox is checked, display the output text
+    if (myDrop.style.diplay == "none" && checkBox.checked == true) {
+      myDrop.style.display = "block";
+    }
+    else {
+      myDrop.style.display = "none";
+    }
   }
-  // If the checkbox is checked, display the output text
- 
-  if (myDrop.style.diplay == "none" & checkBox.checked == true) {
-    myDrop.style.display = "block";
-  }
-  else {
-    myDrop.style.display = "none";
-  }
-}
